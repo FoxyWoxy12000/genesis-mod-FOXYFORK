ffd363a401a79d0de3bbc441cc2bafe53a0cde5e
FoxyWoxy12000 <140989615+FoxyWoxy12000@users.noreply.github.com>
2026-06-19 20:48:26 -0700

    fixed merge request being DUMBBBBBBB

--- src/main/kotlin/dev/d4vid/mods/genesis/server/Genesis.kt
+++ src/main/kotlin/dev/d4vid/mods/genesis/server/Genesis.kt
@@ -9,8 +9,6 @@ import net.fabricmc.api.DedicatedServerModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
-
 @Suppress("unused")
 object Genesis : DedicatedServerModInitializer {
     const val MOD_ID = "genesis"
